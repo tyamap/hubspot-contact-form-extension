14434565045e3e352927214288001f5f20fc716d
tyamap <tom333my@outlook.com>
2023-08-24 23:57:09 +0900

    feat: 認証フローの実装

--- options/index.tsx
+++ options/index.tsx
@@ -4,11 +4,31 @@ const Options = () => {
   const handleClickAuth = () => {
     chrome.identity.launchWebAuthFlow(
       {
-        url: `https://app.hubspot.com/oauth/authorize?client_id=${process.env.HUBSPOT_CLIENT_ID}&redirect_uri=${process.env.HUBSPOT_REDIRECT_URL}&scope=${HUBSPOT_SCOPE}`,
+        url: `https://app.hubspot.com/oauth/authorize?client_id=${process.env.PLASMO_PUBLIC_HUBSPOT_CLIENT_ID}&redirect_uri=${process.env.PLASMO_PUBLIC_HUBSPOT_REDIRECT_URL}&scope=${HUBSPOT_SCOPE}`,
         interactive: true
       },
       (responseUrl) => {
-        // 後述
+        let url = new URL(responseUrl)
+        let code = url.searchParams.get("code")
+
+        fetch("https://api.hubapi.com/oauth/v1/token", {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            grant_type: "authorization_code",
+            client_id: process.env.PLASMO_PUBLIC_HUBSPOT_CLIENT_ID,
+            client_secret: process.env.PLASMO_PUBLIC_HUBSPOT_CLIENT_SECRET,
+            redirect_uri: process.env.PLASMO_PUBLIC_HUBSPOT_REDIRECT_URL,
+            code: code
+          })
+        })
+          .then((res) => res.json())
+          .then((json) => {
+            const token = json["token"] // アクセストークン取得
+            alert(token)
+          })
       }
     )
   }
