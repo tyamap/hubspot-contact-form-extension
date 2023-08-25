cc157b1330f4321c4b1351b3156a3e9c9e762767
tyamap <tom333my@outlook.com>
2023-08-26 00:01:05 +0900

    feat: リフレッシュトークンの使用

--- options/index.tsx
+++ options/index.tsx
@@ -1,5 +1,15 @@
+import { useStorage } from "@plasmohq/storage/hook"
+
+type Tokens = {
+  accessToken?: string
+  refreshToken?: string
+  expiredAt?: string
+}
+
 const Options = () => {
   const HUBSPOT_SCOPE = "crm.objects.contacts.read%20crm.objects.contacts.write"
+
+  const [tokens, setTokens] = useStorage<Tokens>("tokens")
 
   const handleClickAuth = () => {
     chrome.identity.launchWebAuthFlow(
@@ -21,10 +31,6 @@ const Options = () => {
           "client_secret",
           process.env.PLASMO_PUBLIC_HUBSPOT_CLIENT_SECRET
         )
-        formData.append(
-          "redirect_uri",
-          process.env.PLASMO_PUBLIC_HUBSPOT_REDIRECT_URL
-        )
         formData.append("code", code)
 
         fetch("https://api.hubapi.com/oauth/v1/token", {
@@ -36,11 +42,55 @@ const Options = () => {
           .then((json) => {
             const refreshToken = json["refresh_token"]
             const accessToken = json["access_token"]
-            console.log(refreshToken)
-            console.log(accessToken)
+            const expiresIn = json["expires_in"]
+            // トークン期限を設定
+            const expiredAt = expiresIn
+              ? new Date(new Date().getTime() + expiresIn * 1000)
+              : new Date()
+            setTokens({
+              refreshToken,
+              accessToken,
+              expiredAt: expiredAt.toString()
+            })
           })
       }
     )
+  }
+
+  const handleClickRefresh = () => {
+    // 現在の日時とアクセストークンの有効期限を比較して、有効かどうか判定
+    // HubSpot のリフレッシュトークンは失効しない
+    const isExpired = new Date() > new Date(tokens.expiredAt)
+    console.log("期限切れ？", isExpired)
+
+    const formData = new URLSearchParams()
+    formData.append("grant_type", "refresh_token")
+    formData.append("client_id", process.env.PLASMO_PUBLIC_HUBSPOT_CLIENT_ID)
+    formData.append(
+      "client_secret",
+      process.env.PLASMO_PUBLIC_HUBSPOT_CLIENT_SECRET
+    )
+    formData.append("refresh_token", tokens.refreshToken)
+    fetch("https://api.hubapi.com/oauth/v1/token", {
+      method: "post",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: formData
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        const refreshToken = json["refresh_token"]
+        const accessToken = json["access_token"]
+        const expiresIn = json["expires_in"]
+        // トークン期限を設定
+        const expiredAt = expiresIn
+          ? new Date(new Date().getTime() + expiresIn * 1000)
+          : new Date()
+        setTokens({
+          refreshToken,
+          accessToken,
+          expiredAt: expiredAt.toString()
+        })
+      })
   }
 
   return (
@@ -50,7 +100,13 @@ const Options = () => {
         flexDirection: "column",
         padding: 16
       }}>
+      <button onClick={handleClickRefresh}>トークンリフレッシュ</button>
       <button onClick={handleClickAuth}>HubSpot認証</button>
+      <ul>
+        <li>access_token: {tokens?.accessToken}</li>
+        <li>refresh_token: {tokens?.refreshToken}</li>
+        <li>expired_at: {tokens?.expiredAt}</li>
+      </ul>
     </div>
   )
 }
