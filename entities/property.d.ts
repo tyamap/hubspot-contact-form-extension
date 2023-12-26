f1329bf983a25de8cafc123651979a35df79e8ed
tyamap <tom333my@outlook.com>
2023-12-26 20:51:13 +0900

    feat: プロパティの表示UI

--- entities/property.d.ts
+++ entities/property.d.ts
@@ -1,5 +1,32 @@
 type Property = {
-  name: string
-  label: string
+  calculated: boolean
   description: string
+  displayOrder: number
+  externalOptions: boolean
+  fieldType: string
+  formField: boolean
+  groupName: string
+  hasUniqueValue: boolean
+  hidden: boolean
+  hubspotDefined: boolean
+  label: string
+  archivable: boolean
+  readOnlyDefinition: boolean
+  readOnlyValue: boolean
+  name: string
+  type: string
+  modificationMetadata: {
+    archivable: boolean
+    readOnlyDefinition: boolean
+    readOnlyValue: boolean
+  }
+  options: {
+    description: string
+    displayOrder: number
+    hidden: boolean
+    label: string
+    value: string
+  }[]
+  createdAt: string
+  updatedAt: string
 }
--- options/index.tsx
+++ options/index.tsx
@@ -1,3 +1,5 @@
+import "~/style.css"
+
 import axios from "axios"
 import { useState } from "react"
 
@@ -6,11 +8,15 @@ import { useStorage } from "@plasmohq/storage/hook"
 import type { Tokens } from "~entities/tokens"
 import { refreshAuthToken } from "~lib/auth"
 
+type PropertyGroup = {
+  [groupName: string]: Property[]
+}
+
 const Options = () => {
   const HUBSPOT_SCOPE = "crm.objects.contacts.read%20crm.objects.contacts.write"
 
   const [tokens, setTokens] = useStorage<Tokens>("tokens")
-  const [properties, setProperties] = useState<Property[]>([])
+  const [propertyGroups, setPropertyGroups] = useState<PropertyGroup>({})
 
   const handleClickAuth = () => {
     // 自身のChrome拡張IDを含んだURLをリダイレクト先とする
@@ -81,7 +87,14 @@ const Options = () => {
       .post(url, data, { headers })
       .then((res) => {
         console.log(res)
-        setProperties(res.data.results)
+
+        const result = res.data.results.reduce((group, p) => {
+          group[p.groupName] = group[p.groupName] ?? []
+          group[p.groupName].push(p)
+          return group
+        }, {})
+        console.log(result)
+        setPropertyGroups(result)
       })
       .catch((e) => {
         console.error(e)
@@ -98,15 +111,29 @@ const Options = () => {
       {tokens?.refreshToken ? (
         <div>
           <div>認証済み</div>
-          <button onClick={onClick}>プロパティー取得</button>
-          <table>
-            {properties.map((prop) => (
-              <tr key={prop.name}>
-                <td>{prop.label}</td>
-                <td>{prop.name}</td>
-              </tr>
+          <button
+            onClick={onClick}
+            className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mb-4">
+            プロパティー取得
+          </button>
+          <div className="max-w-4xl m-auto">
+            {Object.keys(propertyGroups).map((groupName) => (
+              <div key={groupName || "no_group"}>
+                <h2 className="text-lg font-bold mb-2">{groupName || "no_group"}</h2>
+                <div className="flex flex-wrap">
+                  {propertyGroups[groupName].map((prop) => (
+                    <div key={prop.name} className="w-1/2 mb-4">
+                      <label>
+                        <input type="checkbox" className="align-[-2px] mr-1" />
+                        <span className="font-bold">{prop.label}</span>
+                      </label>
+                      <p className="pl-4 pr-4">{prop.description}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
             ))}
-          </table>
+          </div>
         </div>
       ) : (
         <button onClick={handleClickAuth}>HubSpot認証</button>
